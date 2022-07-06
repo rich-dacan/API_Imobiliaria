3ea4353dceacb1a2905fe8de883a2fea03ec98ec
Richard Cândido <rich_dacan@protonmail.com>
2022-07-06 14:48:14 -0300

    feat :sparkles: created conditional that restricts to listing all users

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import {createUserController, listUsersController} from "../controllers/user.controller";
+import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
 
 const userRouters = Router();
 
 userRouters.post("", createUserController);
-userRouters.get("", listUsersController);
+userRouters.get("", ensureAuthMiddleware, listUsersController);
 
 export default userRouters;
