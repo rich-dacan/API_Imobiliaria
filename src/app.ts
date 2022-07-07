15c08f4d372459ad8678aa4b4aceada996941054
Richard Cândido <rich_dacan@protonmail.com>
2022-07-07 07:42:03 -0300

    refactor :hammer: added a global error handling library, created global middleware, and refactored all related controllers and services

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import "reflect-metadata";
+import "express-async-errors";
 import express from "express";
+
+import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware";
 
 import userRouters from "./routers/user.router";
 import sessionRoutes from "./routers/session.routes";
@@ -10,6 +13,8 @@ app.use(express.json());
 
 app.use("/users", userRouters);
 app.use("/login", sessionRoutes);
+
+app.use(handleAppErrorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}! 🚀`);
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,21 +4,13 @@ import deleteUserService from "../services/users/deleteUser.service";
 import listUsersService from "../services/users/lisUsers.service";
 
 const createUserController = async (req:Request, res:Response) => {
-  try {
-    const { nome, email, password, adm } = req.body;
 
-    const newUser = await createUserService({nome, email, password, adm});
+  const { nome, email, password, adm } = req.body;
 
-    return res.status(201).json(newUser);
+  const newUser = await createUserService({nome, email, password, adm});
 
-  } catch (error) {
-    if (error instanceof Error){
+  return res.status(201).json(newUser);
 
-      return res.status(400).json({
-        "message": error.message
-      })
-    }
-  }
 }
 
 const listUsersController = async (req:Request, res:Response) => {
@@ -29,21 +21,13 @@ const listUsersController = async (req:Request, res:Response) => {
 }
 
 const deleteUserController = async (req:Request, res:Response) => {
-  try {
-    const userId = req.params.userId;
 
-    await deleteUserService(userId);
+  const userId = req.params.userId;
 
-    return res.status(204).send();
+  await deleteUserService(userId);
 
-  } catch (error) {
-    if (error instanceof Error) {
+  return res.status(204).send();
 
-      return res.status(400).json({
-        "message": error.message
-      })
-    }
-  }
 }
 
 export {listUsersController, createUserController, deleteUserController }
